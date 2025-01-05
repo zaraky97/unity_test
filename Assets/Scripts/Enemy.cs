eacb0a2c44bb541728aa4d90fc43f406757f6230
DESKTOP-JTH1EPQ\sasag <yuya.sasagawa@item-shop.jp>
2025-01-06 02:54:24 +0900

    途中まで

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Enemy : MonoBehaviour
 {
@@ -6,6 +7,11 @@ public class Enemy : MonoBehaviour
     private float _moveSpeed;
     [SerializeField, Header("çUåÇóÕ")]
     private int _attackPower;
+    private Vector2 _inputDirection;
+    private float _direction = 1f;
+
+    private float _floorMinX;
+    private float _floorMaxX;
 
     private Rigidbody2D _rigid;
 
@@ -13,6 +19,10 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         _rigid = GetComponent<Rigidbody2D>(); // Get Rigidbody2D component
+        GameObject floor = GameObject.Find("Floor");
+        Debug.Log(floor.transform.lossyScale.x);
+        _floorMinX =(floor.transform.lossyScale.x / 2) * -1 + 0.2f;
+        _floorMaxX = floor.transform.lossyScale.x / 2 - 0.2f;
 
     }
 
@@ -20,15 +30,44 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         _Move();
+        _FlipByHorizontalDirection();
     }
 
     private void _Move()
     {
-        _rigid.linearVelocity = new Vector2(Vector2.left.x * _moveSpeed, _rigid.linearVelocityY);
+        Debug.Log(_direction);
+        _rigid.linearVelocity = new Vector2((_direction > 0 ? Vector2.left.x : Vector2.right.x) * _moveSpeed, _rigid.linearVelocityY);
+        if (transform.position.x <= _floorMinX)
+        {
+            _direction = -1f;
+        }
+        else if (transform.position.x >= _floorMaxX)
+        {
+            _direction = 1f;
+        }
     }
+
 
     public void PlayerDamage(Player player)
     {
         player.Damage(_attackPower);
+    }
+
+    private void _FlipByHorizontalDirection()
+    {
+        if (_direction > 0)
+        {
+            transform.localScale = new Vector2(
+                Mathf.Abs(transform.localScale.x),
+                transform.localScale.y
+            );
+        }
+        else if (_direction < 0)
+        {
+            transform.localScale = new Vector2(
+                -Mathf.Abs(transform.localScale.x),
+                transform.localScale.y
+            );
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,34 +7,69 @@ public class Player : MonoBehaviour
     private float _moveSpeed;
     [SerializeField, Header("ジャンプ速度")]
     private float _jumpSpeed;
+    [SerializeField, Header("体力")]
+    private int _hp;
 
     private Vector2 _inputDirection;
     private Rigidbody2D _rigid;
+    private Animator _anim;
     private bool _bJump;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rigid = GetComponent<Rigidbody2D>(); // Get Rigidbody2D component
+        _anim = GetComponent<Animator>(); // Get Animator component
         _bJump = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         _Move();
+        _FlipByHorizontalDirection();
     }
 
     private void _Move()
     {
         _rigid.linearVelocity = new Vector2(_inputDirection.x * _moveSpeed, _rigid.linearVelocityY);
+        _anim.SetBool("Walk", _inputDirection.x != 0.0f);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Floor") // When the player collides with the ground
+        if (collision.gameObject.tag == "Floor")
         {
             _bJump = false;
+            _anim.SetBool("Jump", _bJump);
+        }
+        if (collision.gameObject.tag == "Enemy")
+        {
+            _HitEnemy(collision.gameObject);
+        }
+    }
+
+    private void _HitEnemy(GameObject enemy)
+    {
+        float halfScaleY = transform.lossyScale.y / 2.0f;
+        float enemyHalfScaleY = enemy.transform.lossyScale.y / 2.0f;
+        if (transform.position.y - (halfScaleY - 0.1f) >= enemy.transform.position.y + (enemyHalfScaleY - 0.1f))
+        {
+            Destroy(enemy);
+            _rigid.AddForce(Vector2.up * (_jumpSpeed / 2), ForceMode2D.Impulse);
+        }
+        else
+        {
+            enemy.GetComponent<Enemy>().PlayerDamage(this);
+        }
+    }
+
+    private void _Dead()
+    {
+        if (_hp <= 0)
+        {
+            Destroy(gameObject);
         }
     }
 
@@ -45,8 +80,38 @@ public class Player : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (!context.performed || _bJump) return; // When the button is pressed
-        _rigid.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse); // Add force to the rigidbody
+        if (!context.performed || _bJump) return;
+        _rigid.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
         _bJump = true;
+        _anim.SetBool("Jump", _bJump);
+    }
+
+    public void Damage(int damage)
+    {
+        _hp = Mathf.Max(_hp - damage, 0);
+        _Dead();
+    }
+
+    public int GetHp()
+    {
+        return _hp;
+    }
+
+    private void _FlipByHorizontalDirection()
+    {
+        if (_inputDirection.x > 0)
+        {
+            transform.localScale = new Vector2(
+                Mathf.Abs(transform.localScale.x),
+                transform.localScale.y
+            );
+        }
+        else if (_inputDirection.x < 0)
+        {
+            transform.localScale = new Vector2(
+                -Mathf.Abs(transform.localScale.x),
+                transform.localScale.y
+            );
+        }
     }
 }
